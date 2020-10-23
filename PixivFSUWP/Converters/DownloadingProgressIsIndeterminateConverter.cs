3b3417ce0674c94b22866fa653b4b7404a16cbfc
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-10-23 11:52:46 +0800

    升级Nuget包 加入两个值转换器

---
+++ PixivFSUWP/Converters/DownloadingProgressIsIndeterminateConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Windows.UI.Xaml.Data;
+
+namespace PixivFSUWP.Converters
+{
+    public class DownloadingProgressIsIndeterminateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+            => value is int i && i <= 0;
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => null;
+    }
+}
---
+++ PixivFSUWP/Converters/DownloadingProgressVisibilityConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace PixivFSUWP.Converters
+{
+    public class DownloadingProgressVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+            => value is int i && i == 100 ? Visibility.Collapsed : (object)Visibility.Visible;
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => null;
+    }
+}
